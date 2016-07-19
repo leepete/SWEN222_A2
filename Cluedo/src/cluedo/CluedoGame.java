2774aec056b3e404f6657da2e4e3a1833208c188
Darren <DarrenHobern@noreply.github.com>
2016-07-19 22:07:55 +1200

    Finished point, worked on room class

--- Cluedo/src/cluedo/CluedoGame.java
+++ Cluedo/src/cluedo/CluedoGame.java
@@ -1,10 +1,14 @@
 package cluedo;
+import java.util.List;
+import java.util.ArrayList;
 
 public class CluedoGame {
 
+	List<Players> players = new ArrayList<Players>();
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
 	}
 
 }
