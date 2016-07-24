a419604e516b367d8fde91cd7ccb4d89580fbdd3
peterly <peterly@hotmail.co.nz>
2016-07-24 18:45:01 +1200

    started 25x25 board

--- Cluedo/src/cluedo/CluedoGame.java
+++ Cluedo/src/cluedo/CluedoGame.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class CluedoGame {
 
 	List<Players> players = new ArrayList<Players>();
+	private int[][] cBoard; // 25x25 board
 	
 	/*Just Start title when you run game*/
 	public static void asciiStart(){
@@ -21,6 +22,11 @@ public class CluedoGame {
 		draw += "▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌" + "\n";
 		draw += " ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀▀▀ " + "\n";                               
 		System.out.println(draw);
+	}
+	
+	public void board(){
+		cBoard = new int[24][24];
+		
 	}
 	
 	public static void main(String[] args) {
