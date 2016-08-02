459ec04e5d9176f81798449fdd750278fbfdc7cc
peterly <peterly@hotmail.co.nz>
2016-08-03 05:44:21 +1200

    ffs

--- Cluedo/src/cluedo/Board.java
+++ Cluedo/src/cluedo/Board.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -14,12 +15,12 @@ public class Board {
 	private static final int BOARD_WIDTH = 25;
 	private static final int BOARD_HEIGHT = 25;
 	
-	private char[][] cBoard; // = new int[BOARD_WIDTH][BOARD_HEIGHT];
+	private int[][] cBoard; // = new int[BOARD_WIDTH][BOARD_HEIGHT];
 	List<Players> players = new ArrayList<Players>(); //real players ingame
 	List<Weapon> weapons = new ArrayList<Weapon>(); 
 	
 	public Board() throws IOException {
-		cBoard  = new char[BOARD_WIDTH][BOARD_HEIGHT];
+		cBoard  = new int[BOARD_WIDTH][BOARD_HEIGHT];
 		BufferedReader br = null;
 		try{
 			br = new BufferedReader(new FileReader(new File("cluemap.txt")));
@@ -30,17 +31,19 @@ public class Board {
 			
 			
 			while((line = br.readLine()) != null){
-				System.out.println(line);
+				//System.out.println(line);
 				
-				//String[] token = line.split("");
-				char[] c = line.toCharArray();
+				String[] c = line.split("");
+				//int[] c = line.toCharArray();
 				
-				
-				for(int i = 0; i < cBoard.length; i++){
-					for(int j = 0; j < cBoard.length; j++){
-						//cBoard[i][j] = Integer.parseInt(c[i]);
-						cBoard[i++] = line.toCharArray();
+				for(int j = 0; j < cBoard.length; j++){
+					for(int i = 0; i < cBoard[i].length-1; i++){
+						//for(int j = 0; j < cBoard[i].length; j++){
+						cBoard[j][i] = Integer.parseInt(c[i]);
+						//cBoard[j++] = line.toCharArray();
+						System.out.println(Arrays.toString(c));
 					}
+					System.out.println();
 				}
 			}
 
