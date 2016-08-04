20ff5f1689f0518fb7836ed8a041703a62dba645
DarrenHobern <darren.hobern@gmail.com>
2016-08-04 22:46:53 +1200

    Started cluedo game

--- src/cluedo/Board.java
+++ src/cluedo/Board.java
@@ -13,43 +13,44 @@ import java.util.List;
 
 public class Board {
 		
-	private static final int BOARD_WIDTH = 25;
-	private static final int BOARD_HEIGHT = 25;
+	private static final int BOARD_WIDTH = 26;
+	private static final int BOARD_HEIGHT = 27;
 	
-	private int[][] cBoard; // = new int[BOARD_WIDTH][BOARD_HEIGHT];
-	List<Player> players = new ArrayList<Player>(); //real players ingame
-	List<Weapon> weapons = new ArrayList<Weapon>(); 
+	private char[][] board = { "---------------------------".toCharArray(),
+							   "|---------S----S----------|".toCharArray(),
+							   "|     T#+++     +++#      |".toCharArray(),
+							   "|      ++         ++  Con |".toCharArray(),
+							   "|  Kit ++    Ball ++      |".toCharArray(),
+							   "|      ++         ++◀     |".toCharArray(),
+							   "|      ++◀       ▶+++   T#|".toCharArray(),
+							   "|#   ▼ ++         +++++++S|".toCharArray(),
+							   "|++++++++ ▼     ▼ +++++++#|".toCharArray(),
+							   "|#++++++++++++++++++      |".toCharArray(),
+							   "|        ++~~~~~~+++d bil |".toCharArray(),
+							   "|        ++~    ~+++      |".toCharArray(),
+							   "|  din  ▶++~    ~+++    ▼ |".toCharArray(),
+							   "|        ++~  I ~++++++++#|".toCharArray(),
+							   "|        ++~    ~+++  ▲  #|".toCharArray(),
+							   "|      ▼ ++~    ~++       |".toCharArray(),
+							   "|#+++++++++~~~~~~++◀  Lib |".toCharArray(),
+							   "|S+++++++++++++++++       |".toCharArray(),
+							   "|#++++++++  ▲▲   +++     #|".toCharArray(),
+							   "|T     ▲++       ++++++++S|".toCharArray(),
+							   "|       ++      ▶++++++++#|".toCharArray(),
+							   "|       ++  hall ++▲     T|".toCharArray(),
+							   "|  lou  ++       ++       |".toCharArray(),
+							   "|       ++       ++  sdy  |".toCharArray(),
+							   "|      #S#       #+       |".toCharArray(),
+							   "---------------------------".toCharArray()};
 	
 	public Board(){
-		String board = "---------------------------\n" +
-					   "|■■■■■■■■■S■■■■S■■■■■■■■■|\n" +
-					   "|     T#⛶⛶⛶     +++#      |\n" +
-					   "|      ++         ++  Con |\n" +
-					   "|  Kit ++    Ball ++      |\n" +
-					   "|      ++         ++◀     |\n" +
-					   "|      ++◀       ▶+++   T#|\n" +
-					   "|#   ▼ ++         +++++++S|\n" +
-					   "|++++++++ ▼     ▼ +++++++#|\n" +
-					   "|#++++++++++++++++++      |\n" +
-					   "|        ++~~~~~~+++d bil |\n" +
-					   "|        ++~    ~+++      |\n" +
-					   "|  din  ▶++~    ~+++    ▼ |\n" +
-					   "|        ++~  I ~++++++++#|\n" +
-					   "|        ++~    ~+++  ▲  #|\n" +
-					   "|      ▼ ++~    ~++       |\n" +
-					   "|#+++++++++~~~~~~++◀  Lib |\n" +
-					   "|S+++++++++++++++++       |\n" +
-					   "|#++++++++  ▲▲   +++     #|\n" +
-					   "|T     ▲++       ++++++++S|\n" +
-					   "|       ++      ▶++++++++#|\n" +
-					   "|       ++  hall ++▲     T|\n" +
-					   "|  lou  ++       ++       |\n" +
-					   "|       ++       ++  sdy  |\n" +
-					   "|      #S#       #+       |\n" +
-					   "---------------------------\n";
-		System.out.println(board);
+		for(int i = 0; i < BOARD_WIDTH; i++) {
+			for(int j = 0; j < BOARD_HEIGHT; j++) {
+				System.out.print(board[i][j]);
+			}
+			System.out.println();
+		}	
 	}
 	
 	
-
 }
--- src/cluedo/CluedoGame.java
+++ src/cluedo/CluedoGame.java
@@ -1,13 +1,104 @@
 package cluedo;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class CluedoGame {
 
+	//List of the players in the game
+	List<Player> players = new ArrayList<Player>();
+	//Number of players in the game
+	int numPlayers;
+	//Mapping character to their name
+	Map<String, Character> availableChars = new HashMap<String, Character>();
+	
 	public CluedoGame() {
 		Board b = new Board();
+		Scanner s = new Scanner(System.in);
+		startGame(s);
 	}
-	List<Player> players = new ArrayList<Player>();
+	
+	/**
+	 * Start of the game method,
+	 * finds the number of players
+	 * who's playing what
+	 * selects the solution
+	 * deals the hands
+	 * resets the board
+	 */
+	public void startGame(Scanner s) {
+		resetGame();
+		
+		//Get number of players
+		
+		
+		
+		int i = 0;
+		while(i < numPlayers) {
+		
+			System.out.println(String.format("Player %d, please enter the character you wish to play as:", i));
+			//Switch find the player they entered and assign that from the set to their player object
+			for(Character c : availableChars.values()) {
+				System.out.println(c.toString());
+			} 
+			String plCharChoice = s.nextLine();
+			if(isValidCharName(plCharChoice)) {
+				switch(plCharChoice) {
+				case "Miss Scarlet": 
+				};
+			} else {
+				System.out.println("That was an invalid player, please enter again:");
+			}
+		}
+	}
+	
+	private boolean isValidCharName(String name) {
+		return availableChars.keySet().contains(name);
+	}
+	
+	/**
+	 * Gets the number of players in the game from the user and ensures that it is between 2 and 6
+	 * @param s
+	 * @return number of players in this game
+	 */
+	private int getNumPlayers(Scanner s) {
+		System.out.println("Welcome to cluedo, please enter the number of people who will be playing (2-6):");
+		// === Check here that the input from the user is actually an integer ===
+		int numPlayers = s.nextInt();
+		while(numPlayers > 6 || numPlayers < 2) {
+			System.out.println("That was an invalid number of players, ensure the number of players is between 2 and 6");
+			numPlayers = s.nextInt();
+		}
+		System.out.println(String.format("Awesome, there will be %d players this game.", numPlayers));
+		return numPlayers;
+	}
+	/**
+	 * Resets all the fields for a new game
+	 */
+	private void resetGame() {
+		resetCharacters();
+		numPlayers = 0;
+	}
+	
+	/**
+	 * Resets the availableCharacters HashSet for a new game
+	 */
+	private void resetCharacters() {
+		//Reset the Map
+		availableChars = new HashMap<String, Character>();
+		//Populate the map with all the options
+		availableChars.put("Miss Scarlet", new Character("Miss Scarlet"));
+		availableChars.put("Colonel Mustard", new Character("Colonel Mustard"));
+		availableChars.put("Mrs. White", new Character("Mrs. White"));
+		availableChars.put("The Reverend Green", new Character("The Reverend Green"));
+		availableChars.put("Mrs. Peacock", new Character("Mrs. Peacock"));
+		availableChars.put("Professor Plum", new Character("Professor Plum"));
+	}
+	
 	
 	public static void main(String[] args) {
 		CluedoGame game = new CluedoGame();
