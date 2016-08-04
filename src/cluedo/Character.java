4840906f2fe171095a076098c202309f12817c2f
DarrenHobern <darren.hobern@gmail.com>
2016-08-04 15:22:55 +1200

    nothign

---
+++ src/cluedo/Character.java
@@ -0,0 +1,12 @@
+package cluedo;
+
+public class Character {
+	String name;
+	public Character(String name) {
+		this.name = name;
+	}
+	
+	public String toString() {
+		return name;
+	}
+}
--- src/cluedo/CluedoGame.java
+++ src/cluedo/CluedoGame.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 
 public class CluedoGame {
 
+	public CluedoGame() {
+		System.out.println("New Game!");
+	}
 	List<Player> players = new ArrayList<Player>();
 	
 	public static void main(String[] args) {
-		
-		
+		CluedoGame game = new CluedoGame();	
 	}
 
 }
