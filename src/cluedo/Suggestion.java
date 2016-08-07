5b2cfd3d82d08c6021dbbc843e32081258ca3731
DarrenHobern <darren.hobern@gmail.com>
2016-08-07 20:07:29 +1200

    merge suggestion with peter

--- src/cluedo/Suggestion.java
+++ src/cluedo/Suggestion.java
@@ -1,17 +1,29 @@
 package cluedo;
 
 public class Suggestion {
-	
-	Weapon weapon;
-	Character character;
-	Room room;
-	
-	public Suggestion(Weapon weapon, Character character, Room room) {
+
+	String weapon;
+	String character;
+	String room;
+
+	public Suggestion(String room, String weapon, String character) {
 		this.weapon = weapon;
 		this.room = room;
 		this.character = character;
 	}
-	
+
+	public boolean isValid() {
+		return (this.room != null && this.character != null && this.weapon != null);
+	}
+
+	public boolean equals(Suggestion s) {
+		if(s == null){
+			return false;
+		}	
+		return (this.weapon.equals(s.weapon) && this.character.equals(s.character) && this.room.equals(s.room));
+			
+	}
+
 	public String toString() {
 		return String.format("W: %s C: %s R: %s", weapon.toString(), character.toString(), room.toString() );
 	}
